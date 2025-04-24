d07a46b3bf42a447bbd32f3b64bb485d783d430b
avinashLam <101968733+avinashLam@users.noreply.github.com>
2025-04-24 10:50:42 +0530

    inital commit

--- BookingService/src/main/java/com/example/BookingService/repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.BookingService.repository;
-
-import com.example.BookingService.entity.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookingRepository extends JpaRepository<Booking, Integer> {
-}
